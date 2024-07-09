97fac49dc88f91d5f268802c1f6d63298dbcd0b8
Elliot <elliotgriffiss@gmail.com>
2024-07-09 19:57:54 +0100

    Added more clouds

--- src/clouds/clouds.js
+++ src/clouds/clouds.js
@@ -25,10 +25,14 @@ export class Clouds extends Base {
 
         this._cloud1 = PIXI.Sprite.from("cloud1");
         this._cloud2 = PIXI.Sprite.from("cloud2");
+        this._cloud3 = PIXI.Sprite.from("cloud1");
+        this._cloud4 = PIXI.Sprite.from("cloud2");
 
-        Tween.fromTo(this._cloud1, 60000, {x: -400}, {x: 1080, repeat: -1});
-        Tween.fromTo(this._cloud2, 60000, {x: -400}, {x: 1080, repeat: -1, delay: 30});
+        Tween.fromTo(this._cloud1, 80000, {x: -400}, {x: 1080, repeat: -1});
+        Tween.fromTo(this._cloud2, 80000, {x: -400}, {x: 1080, repeat: -1, delay: 20});
+        Tween.fromTo(this._cloud3, 80000, {x: -400}, {x: 1080, repeat: -1, delay: 40});
+        Tween.fromTo(this._cloud4, 80000, {x: -400}, {x: 1080, repeat: -1, delay: 60});
 
-        this._native.addChild(this._cloud1, this._cloud2);
+        this._native.addChild(this._cloud1, this._cloud2, this._cloud3);
     }
 }
